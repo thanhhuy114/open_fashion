a7eb36761093619424a098a0e193bd7addd8b8a2
ThachWynn <146806999+nguyenngocthach1403@users.noreply.github.com>
2024-03-08 10:32:24 +0700

    Thach 8/3 10:31 - Update Add Address Page

---
+++ lib/presentation/add_address_page/bloc/add_address_form/add_address_state.dart
@@ -0,0 +1,94 @@
+part of 'add_address_bloc.dart';
+
+/*
+Modifier By Thach
+Date: 8/3 10:23
+Content:
+  - Change firstNameTextfield -> isValidFirstName
+  - Change lastNameTextfield -> isValidLasName
+  - Change addressTextfield -> isValidAddress
+  - Change cityTextfield -> isValidCity
+  - Change stateTextfield -> isValidState
+  - Change zipCodeTextfield -> isValidZipCode
+  - Change phoneNumberTextfield -> isValidPhoneNumber
+
+ */
+class AddAddressState extends Equatable {
+  const AddAddressState({
+    this.isValidFirstName = false,
+    this.isValidLastName = false,
+    this.isValidAddress = false,
+    this.isValidCity = false,
+    this.isValidState = false,
+    this.isValidZipCode = false,
+    this.isValidPhoneNumber = false,
+    this.firstNameMessage = '',
+    this.lastNameMessage = '',
+    this.addressMessage = '',
+    this.cityMessage = '',
+    this.stateMessage = '',
+    this.zipCodeMessage = '',
+    this.phoneNumberMessage = '',
+  });
+  final bool isValidFirstName;
+  final bool isValidLastName;
+  final bool isValidAddress;
+  final bool isValidCity;
+  final bool isValidState;
+  final bool isValidZipCode;
+  final bool isValidPhoneNumber;
+  final String firstNameMessage;
+  final String lastNameMessage;
+  final String addressMessage;
+  final String cityMessage;
+  final String stateMessage;
+  final String zipCodeMessage;
+  final String phoneNumberMessage;
+
+  AddAddressState copyWith({
+    final bool? isValidFirstName,
+    final bool? isValidLastName,
+    final bool? isValidAddress,
+    final bool? isValidCity,
+    final bool? isValidState,
+    final bool? isValidZipCode,
+    final bool? isValidPhoneNumber,
+    final String? firstNameMessage,
+    final String? lastNameMessage,
+    final String? addressMessage,
+    final String? cityMessage,
+    final String? stateMessage,
+    final String? zipCodeMessage,
+    final String? phoneNumberMessage,
+  }) {
+    return AddAddressState(
+      isValidFirstName: isValidFirstName ?? this.isValidFirstName,
+      isValidLastName: isValidLastName ?? this.isValidLastName,
+      isValidAddress: isValidAddress ?? this.isValidAddress,
+      isValidCity: isValidCity ?? this.isValidCity,
+      isValidPhoneNumber: isValidPhoneNumber ?? this.isValidPhoneNumber,
+      isValidState: isValidState ?? this.isValidState,
+      isValidZipCode: isValidZipCode ?? this.isValidZipCode,
+      firstNameMessage: firstNameMessage ?? this.firstNameMessage,
+      lastNameMessage: lastNameMessage ?? this.lastNameMessage,
+      addressMessage: addressMessage ?? this.addressMessage,
+      cityMessage: cityMessage ?? this.cityMessage,
+      stateMessage: stateMessage ?? this.stateMessage,
+      zipCodeMessage: zipCodeMessage ?? this.zipCodeMessage,
+      phoneNumberMessage: phoneNumberMessage ?? this.phoneNumberMessage,
+    );
+  }
+
+  @override
+  List<Object> props() => [
+        isValidFirstName,
+        isValidLastName,
+        isValidAddress,
+        isValidCity,
+        isValidState,
+        isValidZipCode,
+        isValidPhoneNumber,
+      ];
+}
+
+class AddAddressInitial extends AddAddressState {}
---
+++ lib/presentation/add_address_page/view/add_address_form.dart
@@ -0,0 +1,312 @@
+/*
+  Create by: Thach
+  Date: 9:00 8/3
+  Content: Giao dien add address text field
+*/
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../bloc/add_address_form/add_address_bloc.dart';
+import '../widget/show_validator.dart';
+
+class AddAdressForm extends StatefulWidget {
+  const AddAdressForm({super.key});
+
+  @override
+  State<AddAdressForm> createState() => _AddAdressFormState();
+}
+
+class _AddAdressFormState extends State<AddAdressForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 20,
+            vertical: 15,
+          ),
+          child: Row(
+            children: [
+              Flexible(
+                child: Column(
+                  children: [
+                    TextField(
+                      onChanged: (final value) => context
+                          .read<AddAddressBloc>()
+                          .add(CheckFirstNameTextField(text: value)),
+                      style: Theme.of(context).textTheme.titleSmall!.copyWith(
+                            color: Colors.black,
+                          ),
+                      decoration: InputDecoration(
+                        hintText: 'First Name',
+                        hintStyle:
+                            Theme.of(context).textTheme.titleSmall!.copyWith(
+                                  color: const Color.fromRGBO(
+                                    151,
+                                    151,
+                                    151,
+                                    1,
+                                  ),
+                                ),
+                      ),
+                    ),
+                    BlocBuilder<AddAddressBloc, AddAddressState>(
+                      builder: (final context, final state) {
+                        return ShowValidator(
+                          isValid: state.isValidFirstName,
+                          validate: state.firstNameMessage,
+                        );
+                      },
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                width: 20,
+              ),
+              Flexible(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    TextField(
+                      onChanged: (final value) => context
+                          .read<AddAddressBloc>()
+                          .add(CheckLastNameTextField(text: value)),
+                      style: Theme.of(context).textTheme.titleSmall!.copyWith(
+                            color: Colors.black,
+                          ),
+                      decoration: InputDecoration(
+                        hintText: 'Last Name',
+                        hintStyle:
+                            Theme.of(context).textTheme.titleSmall!.copyWith(
+                                  color: const Color.fromRGBO(
+                                    151,
+                                    151,
+                                    151,
+                                    1,
+                                  ),
+                                ),
+                      ),
+                    ),
+                    BlocBuilder<AddAddressBloc, AddAddressState>(
+                      builder: (final context, final state) {
+                        return ShowValidator(
+                          isValid: state.isValidLastName,
+                          validate: state.lastNameMessage,
+                        );
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+
+        //address
+        Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 20,
+            vertical: 15,
+          ),
+          child: TextField(
+            onChanged: (final value) => context
+                .read<AddAddressBloc>()
+                .add(CheckAddressTextField(text: value)),
+            style: Theme.of(context).textTheme.titleSmall!.copyWith(
+                  color: Colors.black,
+                ),
+            decoration: InputDecoration(
+              hintText: 'Address',
+              hintStyle: Theme.of(context).textTheme.titleSmall!.copyWith(
+                    color: const Color.fromRGBO(
+                      151,
+                      151,
+                      151,
+                      1,
+                    ),
+                  ),
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 20,
+          ),
+          child: BlocBuilder<AddAddressBloc, AddAddressState>(
+            builder: (final context, final state) {
+              return ShowValidator(
+                isValid: state.isValidAddress,
+                validate: state.addressMessage,
+              );
+            },
+          ),
+        ),
+
+        //City
+        Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 20,
+            vertical: 15,
+          ),
+          child: TextField(
+            onChanged: (final value) => context
+                .read<AddAddressBloc>()
+                .add(CheckCityTextField(text: value)),
+            style: Theme.of(context).textTheme.titleSmall!.copyWith(
+                  color: Colors.black,
+                ),
+            decoration: InputDecoration(
+              hintText: 'City',
+              hintStyle: Theme.of(context).textTheme.titleSmall!.copyWith(
+                    color: const Color.fromRGBO(
+                      151,
+                      151,
+                      151,
+                      1,
+                    ),
+                  ),
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 20,
+          ),
+          child: BlocBuilder<AddAddressBloc, AddAddressState>(
+            builder: (final context, final state) {
+              return ShowValidator(
+                isValid: state.isValidCity,
+                validate: state.cityMessage,
+              );
+            },
+          ),
+        ),
+
+        //State and zip code
+        Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 20,
+            vertical: 15,
+          ),
+          child: Row(
+            children: [
+              Flexible(
+                child: Column(
+                  children: [
+                    TextField(
+                      onChanged: (final value) => context
+                          .read<AddAddressBloc>()
+                          .add(CheckStateTextField(text: value)),
+                      style: Theme.of(context).textTheme.titleSmall!.copyWith(
+                            color: Colors.black,
+                          ),
+                      decoration: InputDecoration(
+                        hintText: 'State',
+                        hintStyle:
+                            Theme.of(context).textTheme.titleSmall!.copyWith(
+                                  color: const Color.fromRGBO(
+                                    151,
+                                    151,
+                                    151,
+                                    1,
+                                  ),
+                                ),
+                      ),
+                    ),
+                    BlocBuilder<AddAddressBloc, AddAddressState>(
+                      builder: (final context, final state) {
+                        return ShowValidator(
+                          isValid: state.isValidState,
+                          validate: state.stateMessage,
+                        );
+                      },
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                width: 20,
+              ),
+              Flexible(
+                child: Column(
+                  children: [
+                    TextField(
+                      onChanged: (final value) => context
+                          .read<AddAddressBloc>()
+                          .add(CheckZipCodeTextField(text: value)),
+                      style: Theme.of(context).textTheme.titleSmall!.copyWith(
+                            color: Colors.black,
+                          ),
+                      decoration: InputDecoration(
+                        hintText: 'Zip Code',
+                        hintStyle:
+                            Theme.of(context).textTheme.titleSmall!.copyWith(
+                                  color: const Color.fromRGBO(
+                                    151,
+                                    151,
+                                    151,
+                                    1,
+                                  ),
+                                ),
+                      ),
+                    ),
+                    BlocBuilder<AddAddressBloc, AddAddressState>(
+                      builder: (final context, final state) {
+                        return ShowValidator(
+                          isValid: state.isValidZipCode,
+                          validate: state.zipCodeMessage,
+                        );
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+
+        //phone number
+        Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 20,
+            vertical: 15,
+          ),
+          child: TextField(
+            onChanged: (final value) => context
+                .read<AddAddressBloc>()
+                .add(CheckPhoneTextField(text: value)),
+            style: Theme.of(context).textTheme.titleSmall!.copyWith(
+                  color: Colors.black,
+                ),
+            decoration: InputDecoration(
+              hintText: 'Phone Number',
+              hintStyle: Theme.of(context).textTheme.titleSmall!.copyWith(
+                    color: const Color.fromRGBO(
+                      151,
+                      151,
+                      151,
+                      1,
+                    ),
+                  ),
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 20,
+          ),
+          child: BlocBuilder<AddAddressBloc, AddAddressState>(
+            builder: (final context, final state) {
+              return ShowValidator(
+                isValid: state.isValidPhoneNumber,
+                validate: state.phoneNumberMessage,
+              );
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
