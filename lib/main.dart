9f34a75e4c923f0931c3f0dce7edcfe6a3309489
viehao38 <nguyenhaovkag@gmail.com>
2024-03-08 09:28:08 +0700

    update - viehao

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'presentation/complete_checkout_page/views/complete_checkout.dart';
 import 'widgets/text_theme_custom.dart';
-import 'presentation/home_page/view/home_page_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
         textTheme: TextThemeCustom(),
       ),
-      home:const HomePageScreen(),
+      home: const CompleteCheckoutPage(),
     );
   }
 }
