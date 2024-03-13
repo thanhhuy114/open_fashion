909f381c9eb357435bc881f1f8161181b28d6d3e
ThachWynn <146806999+nguyenngocthach1403@users.noreply.github.com>
2024-03-13 13:33:03 +0700

    Thach 13/3 13:32 update nearest commit

--- lib/presentation/add_cart_page/view/add_card_screen.dart
+++ lib/presentation/add_cart_page/view/add_card_screen.dart
@@ -5,7 +5,6 @@
 */
 
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../widgets/appbar_custom_widget.dart';
