5bd67f0448b8c2eb95e6d2253142fdf7ab66ac04
ThachWynn <146806999+nguyenngocthach1403@users.noreply.github.com>
2024-03-13 13:30:56 +0700

    Thach 13/3 1:30 Preparing warning in my code at card_list , collection detail page

--- lib/presentation/add_cart_page/view/card_list.dart
+++ lib/presentation/add_cart_page/view/card_list.dart
@@ -1,5 +1,3 @@
-import 'package:banner_carousel/banner_carousel.dart';
-import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
--- lib/presentation/collection_detail_page/bloc/collection_detail_bloc/collection_detail_state.dart
+++ lib/presentation/collection_detail_page/bloc/collection_detail_bloc/collection_detail_state.dart
@@ -2,31 +2,15 @@ part of 'collection_detail_bloc.dart';
 
 abstract class CollectionDetailState extends Equatable {
   const CollectionDetailState();
+  @override
+  List<Object?> get props => throw UnimplementedError();
 }
 
-class CollectionDetailLoading extends CollectionDetailState {
-  @override
-  List<Object?> get props {
-    // TODO: implement props
-    throw UnimplementedError();
-  }
-}
+class CollectionDetailLoading extends CollectionDetailState {}
 
 class CollectionDetailLoaded extends CollectionDetailState {
   const CollectionDetailLoaded({required this.colletion});
   final CollectionDetailModel colletion;
-
-  @override
-  List<Object?> get props {
-    // TODO: implement props
-    throw UnimplementedError();
-  }
 }
 
-class CollectionDetailFailure extends CollectionDetailState {
-  @override
-  List<Object?> get props {
-    // TODO: implement props
-    throw UnimplementedError();
-  }
-}
+class CollectionDetailFailure extends CollectionDetailState {}
